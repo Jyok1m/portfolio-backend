1fad8d3613d63a1f2438f37df756357b2f6c1a90
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-12-06 16:22:40 +0100

    added cors compatibility with front

--- app.js
+++ app.js
@@ -10,9 +10,24 @@ var indexRouter = require("./routes/index");
 var inquiryRouter = require("./routes/inquiry.js");
 
 var app = express();
-var cors = require("cors");
 
-app.use(cors());
+var cors = require("cors");
+var corsOptions = {
+	origin: function (origin, callback) {
+		// Replace 'allowedOrigins' with your specific origins for production
+		const allowedOrigins = ["http://localhost:4000", "http://localhost:4001", "https://www.joachimjasmin.fr"];
+		if (allowedOrigins.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+	methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions));
+app.disable("x-powered-by");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
